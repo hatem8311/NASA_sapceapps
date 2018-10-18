003d04816cc8e2bca1c820bd9e4317c22ab1d035
hatem8311 <42862833+hatem8311@users.noreply.github.com>
2018-10-18 22:36:17 +0200

    Update models.py

--- Rockets/homepage/models.py
+++ Rockets/homepage/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 class Rockets_time_table(models.Model):
-    rockets_name = models.CharField(max_length=500)
-    launching_time = models.CharField(max_length=500)
-    mission = models.CharField(max_length=500 ,default='')
-    mission_url=models.CharField(max_length=10000 , default='')
-    descrption=models.CharField(max_length=10000 , default='')
-    image = models.ImageField(upload_to = 'rockets` image' , blank = True)
+    rockets_name = models.CharField(max_length=500 ,default='____')
+    lanuch_window = models.CharField(max_length=10000,default='____')
+    launch_site = models.CharField(max_length=1000,default='____')
+    description = models.CharField(max_length=1000000,default='____')
+    month = models.CharField(max_length = 1000,default='____')
+
+    image = models.ImageField(upload_to = 'rockets` image' ,null=True, blank=True,default='____')
+
+    def  __str__(self):
+        return self.rockets_name
 
 
 
-    ##a field of data of creation
-    #created = models.DateTimeField(auto_now_add=True)
-    ## a field of data of updating
-    #updated = models.DateTimeField(auto_now=True)
+class major_spaceports(models.Model):
+    spaceport_name = models.CharField(max_length=100,default='____')
+    country = models.CharField(max_length=1000,default='____')
+    image= models.ImageField(upload_to = 'spaceport image' , blank=True,default='____')
 
+    def  __str__(self):
+        return self.spaceport_name
 
-# Create your models here.
