17ce0ac31743e8cae315c79c525544a33fa1e062
hatem8311 <42862833+hatem8311@users.noreply.github.com>
2018-10-18 22:37:05 +0200

    Update views.py

--- Rockets/homepage/views.py
+++ Rockets/homepage/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render,redirect
-from homepage.models import Rockets_time_table
+from django.http import HttpResponse as Http
+from homepage.models import Rockets_time_table,major_spaceports
+
 def rockets(request):
-    rockets= Rockets_time_table.objects.all()
+    rockets= Rockets_time_table.objects.all().order_by('id')
     args={'rockets':rockets}
     return render(request, 'homepage/home.html',args)
 
 def spaceports(request):
-    return render(request,'homepage/spaceports.html')    
-
-
-
-
-# Create your views here.
+    spaceports=major_spaceports.objects.all()
+    args={ 'spaceports':spaceports}
+    return render(request,'homepage/spaceports.html' , args)
